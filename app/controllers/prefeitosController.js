f06b6d5fe529dd59fcec6c2b014a9f0a4afc2956
Cleriston Dantas <cleristondantas@MacBook-Air-de-Cleriston.local>
2016-09-21 21:04:21 -0300

    mudando interface para bootstrap

--- app/controllers/prefeitosController.js
+++ app/controllers/prefeitosController.js
@@ -2,12 +2,13 @@ angular.module("eleicoesCatalao")
 .controller("prefeitosCtrl", function ($scope, eleicoesAPI) {
 
 	$scope.urlFoto = "http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/foto/2/";
+	$scope.carregando = true;
 
 	$scope.loadPrefeitos = function (contato) {
 
 		eleicoesAPI.getPrefeitos().success(function (data) {
 			$scope.prefeitos = data.candidatos;
-			console.log(data);
+			$scope.carregando = false;
 		});
 	};
 
--- app/controllers/vereadoresController.js
+++ app/controllers/vereadoresController.js
@@ -2,12 +2,13 @@ angular.module("eleicoesCatalao")
 .controller("vereadoresCtrl", function ($scope, eleicoesAPI) {
 
 	$scope.urlFoto = "http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/foto/2/";
+	$scope.carregando = true;
 
 	$scope.loadVereadores = function (contato) {
 
 		eleicoesAPI.getVereadores().success(function (data) {
 			$scope.vereadores = data.candidatos;
-			console.log(data);
+			$scope.carregando = false;
 		});
 	};
 
